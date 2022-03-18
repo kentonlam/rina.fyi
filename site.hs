027c9073626f548acb55f6e47f4a6f8e82fb01c2
Katrina Lam <k@rina.fyi>
2022-03-18 03:40:51 +0000

    ormolu.

--- site.hs
+++ site.hs
@@ -1,71 +1,73 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
+
+import Data.Monoid (mappend)
+import Hakyll
 import qualified Hakyll.Core.Configuration as Config
 
-
 --------------------------------------------------------------------------------
-
 config :: Config.Configuration
-config = Config.defaultConfiguration { destinationDirectory = "docs" }
+config =
+  Config.defaultConfiguration
+    { destinationDirectory = "docs",
+      providerDirectory = "site"
+    }
 
 main :: IO ()
-main = hakyllWith config $ do
-    match "images/*" $ do
-        route   idRoute
+main = hakyllWith config $
+  do
+    match "images/*" $
+      do
+        route idRoute
         compile copyFileCompiler
-
-    match "css/*" $ do
-        route   idRoute
+    match "css/*" $
+      do
+        route idRoute
         compile copyFileCompiler
-
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
+    match (fromList ["about.rst", "contact.markdown"]) $
+      do
+        route $ setExtension "html"
+        compile $
+          pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
-
-    match "posts/*" $ do
+    match "posts/*" $
+      do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+        compile $
+          pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html" postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
-
-    create ["archive.html"] $ do
+    create ["archive.html"] $
+      do
         route idRoute
-        compile $ do
+        compile $
+          do
             posts <- recentFirst =<< loadAll "posts/*"
             let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
+                  listField "posts" postCtx (return posts)
+                    `mappend` constField "title" "Archives"
+                    `mappend` defaultContext
             makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-
-    match "index.html" $ do
+              >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+              >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+              >>= relativizeUrls
+    match "index.html" $
+      do
         route idRoute
-        compile $ do
+        compile $
+          do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    defaultContext
-
+                  listField "posts" postCtx (return posts)
+                    `mappend` defaultContext
             getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
+              >>= applyAsTemplate indexCtx
+              >>= loadAndApplyTemplate "templates/default.html" indexCtx
+              >>= relativizeUrls
     match "templates/*" $ compile templateBodyCompiler
-
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+postCtx = dateField "date" "%B %e, %Y" `mappend` defaultContext

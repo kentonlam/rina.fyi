7bc903f8b0cb65b6e96665d3f425c2b6079825de
Katrina Lam <k@rina.fyi>
2022-05-23 22:01:50 +1000

    dir.

--- dir/Main.hs
+++ dir/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Monad
@@ -17,8 +19,11 @@ recGlob p f = fromGlob (p <> "/**/" <> f) .||. fromGlob (p <> "/" <> f)
 
 (/**?/) = recGlob
 
+d /./ f = fromGlob (d <> "/" <> f)
+d /*/ f = fromGlob (d <> "/*/" <> f)
+
 adjFiles :: Identifier -> Pattern
-adjFiles i = (fromGlob (d <> "/*.md") .||. fromGlob (d <> "/*/index.md")) .&&. complement (fromList [i])
+adjFiles i = (d /./ "*.md" .||. d /*/ "index.md") .&&. complement (fromList [i])
   where d = dirname (toFilePath i)
 
 dir :: String -> Rules ()
@@ -28,18 +33,31 @@ dir p = do
       route $ setExtension "html"
       compile $ 
         do
-          files <- loadAll <$> adjFiles =<< getUnderlying
+          adj <- adjFiles <$> getUnderlying
+          files <- loadAll (adj)
           -- let f' = filter ((/= f) . toFilePath . itemIdentifier) files
           -- unsafeCompiler $ print files
           getResourceBody 
             >>= applyAsTemplate (listCtx files)
             >>= renderPandoc
           -- makeItem $ f
-  match (p /**?/ "*.md") $ 
+  match (p /**?/ "*.md") $
     do
       route $ setExtension "post.html"
-      compile $
-        getResourceBody
+      compile $ getResourceBody
+
+  -- match (p /**?/ "*.md") $
+  --   do 
+  --     route $ setExtension "html"
+  --     compile $ 
+  --       do
+  --         i <- getUnderlying
+  --         item <- load (setVersion (Just "dir") i) :: Compiler (Item String)
+  --         makeItem $ itemBody item
+  -- create ["index.md"] $
+  --   do
+  --     route $ setExtension "html"
+  --     compile $ makeItem ("make"::String)
 
 main = hakyllWith config (dir "d")
 

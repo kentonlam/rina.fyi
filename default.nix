98c3e9191e738b56148575642ded5f37d40fe43c
rina <k@rina.fyi>
2024-08-18 22:07:32 +1000

    github pages

--- default.nix
+++
@@ -1,7 +0,0 @@
-let
-  pkgs = import <nixpkgs> { }; # pin the channel to ensure reproducibility!
-in
-pkgs.haskellPackages.developPackage {
-  root = pkgs.nix-gitignore.gitignoreSource
-    [ "/docs" ] ./.;
-}
---
+++ flake.nix
@@ -0,0 +1,30 @@
+{ 
+  nixConfig.extra-substituters = [ "https://pac-nix.cachix.org/" ];
+  nixConfig.extra-trusted-public-keys = [ "pac-nix.cachix.org-1:l29Pc2zYR5yZyfSzk1v17uEZkhEw0gI4cXuOIsxIGpc=" ];
+
+  inputs.pac-nix.url = "github:katrinafyi/pac-nix";
+
+  outputs = {self, pac-nix}:
+    let
+      nixpkgs = pac-nix.inputs.nixpkgs;
+
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ] (system: f system pac-nix.legacyPackages.${system});
+
+    in {
+      packages = forAllSystems (sys: pac-nix:
+        let pkgs = nixpkgs.legacyPackages.${sys};
+        in {
+          default = pkgs.haskellPackages.developPackage {
+            root = pkgs.nix-gitignore.gitignoreSource
+              [ "/docs" ] ./.;
+          };
+        }
+      );
+    };
+}
